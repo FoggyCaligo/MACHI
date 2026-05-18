17a53c50259771278afbe250175d73e142e9fc7e
FoggyCaligo <136309172+FoggyCaligo@users.noreply.github.com>
2026-05-18 10:38:21 +0900

    feat: MK6 Goal 패키지 추가

---
+++ MK6/core/goal/__init__.py
@@ -0,0 +1,24 @@
+"""Goal graph support for MK6.
+
+Goal의 본체는 문자열 label이나 node_kind가 아니라 WorldGraph의 node/edge 구조다.
+이 패키지는 그 구조를 초기화하고 조회하는 얇은 레이어만 제공한다.
+"""
+
+from .goal_graph import (
+    GLOBAL_GOAL_AXIS_SEEDS,
+    GOAL_ROOT_HASH,
+    GoalAxisSeed,
+    initialize_global_goal_graph,
+    load_goal_view,
+)
+from .goal_view import GoalAxisRef, GoalView
+
+__all__ = [
+    "GLOBAL_GOAL_AXIS_SEEDS",
+    "GOAL_ROOT_HASH",
+    "GoalAxisSeed",
+    "GoalAxisRef",
+    "GoalView",
+    "initialize_global_goal_graph",
+    "load_goal_view",
+]
