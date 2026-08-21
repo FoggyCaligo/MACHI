1a6de2e6acfafa432fda3f250316e6ecc336cbdd
FoggyCaligo <136309172+FoggyCaligo@users.noreply.github.com>
2026-08-21 14:34:32 +0900

    Apply model tool aliases to injected chat models too

--- MK4/app/pipeline.py
+++ MK4/app/pipeline.py
@@ -69,7 +69,7 @@ class Pipeline:
         self._memory = GraphMemoryService(self._graph_repo)
         self._assistant_memory = AssistantMemoryRecorder(self._graph_repo)
         self._tools = GraphToolSuite(self._memory)
-        base_chat_model = chat_model or ModelToolNameAdapter(OllamaToolChatModel())
+        base_chat_model = ModelToolNameAdapter(chat_model or OllamaToolChatModel())
         self._chat_model = EvidenceGroundingChatModel(AutonomyChatModel(base_chat_model))
         self._web_search = web_search or HttpWebSearchTool()
         self._file_working_roots: dict[str, str] = {}
